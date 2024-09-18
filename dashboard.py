6bd5b816d005a687ab191c8951dcd9fec0a542b9
Anzhe Meng <meng@prepbaseballreport.com>
2024-09-18 11:27:48 -0400

    managed dependencies by poetry

--- dashboard.py
+++ dashboard.py
@@ -1,12 +1,25 @@
 import streamlit as st
-import yaml
+from streamlit_calendar import calendar
+import yaml, sqlite3
 from yaml.loader import SafeLoader
+import pandas as pd
+import plotly.express as px
+from datetime import timedelta, datetime
 
 # Load credentials
 with open('credentials.yaml') as file:
     credentials = yaml.load(file, Loader=SafeLoader)
 
-st.title('Amazing User Login App')
+def time_to_timedelta(time_str):
+    minutes, seconds = map(int, time_str.split(':'))
+    return timedelta(minutes=48) - timedelta(minutes=minutes, seconds=seconds)
+
+st.set_page_config(layout="wide")
+st.markdown('# Clippers Team Management App')
+
+# @st.experimental_memo(ttl=60, persist="disk")
+def retrieve_data(query, conn=sqlite3.connect('lac_fullstack_dev.db')):
+    return pd.read_sql(calendar_query, conn)
 
 # Initialize session state for user
 if 'user_state' not in st.session_state:
@@ -39,14 +52,121 @@ if not st.session_state.user_state['logged_in']:
                 'user_type': user_info['user_type']
             })
             st.success('Successfully logged in!')
-            st.experimental_rerun()  # Refresh the page after successful login
+            st.rerun()  # Refresh the page after successful login
         else:
             st.error('Invalid username or password')
 
 # Once logged in, replace the login screen with the new content
 if st.session_state.user_state['logged_in']:
-    st.title(f"Welcome {st.session_state.user_state['username']}")
-    st.write(f"You are logged in as: {st.session_state.user_state['user_type']}")
+    st.markdown(f"## Welcome {st.session_state.user_state['username']}!")
+    if st.session_state.user_state['user_type'] == 'coach':
+        col1, col2 = st.columns(2)
+
+        calendar_query = """
+        select 
+        CASE WHEN h.teamNameShort = 'LAC' THEN 'vs ' || a.teamNameShort
+        ELSE '@ ' || h.teamNameShort END AS game, 
+        game_date, home_score, away_score
+        from game_schedule gs
+        join team h
+        on gs.home_id = h.teamId
+        join team a
+        on gs.away_id = a.teamId
+        where h.teamNameShort = 'LAC' or a.teamNameShort = 'LAC'
+        """
+
+        gantt_query = """
+        SELECT game_date, 
+        CASE WHEN gs.home_id = t.teamId THEN '@ ' || t.teamNameShort
+        ELSE 'vs ' || t.teamNameShort END AS game,
+        r.first_name || ' ' || r.last_name AS player,
+        printf('%02d%s', 
+        12 * (4 - period) + CAST(SUBSTR(stint_start_time, 1, INSTR(stint_start_time, ':') - 1) AS REAL), 
+        SUBSTR(stint_start_time, INSTR(stint_start_time, ':'), LENGTH(stint_start_time))) AS stint_start_time,
+        printf('%02d%s', 
+        12 * (4 - period) + CAST(SUBSTR(stint_end_time, 1, INSTR(stint_end_time, ':') - 1) AS REAL),
+        SUBSTR(stint_end_time, INSTR(stint_end_time, ':'), LENGTH(stint_end_time))) AS stint_end_time
+        FROM roster r
+        LEFT JOIN stints s
+        ON player_name = r.first_name || ' ' || r.last_name
+        JOIN game_schedule gs
+        ON gs.game_id = s.game_id
+        JOIN team t
+        ON t.teamName = s.opponent
+        WHERE team = 'LA Clippers'
+        AND opponent = 'Miami Heat'
+        """
+
+        calendar_df = retrieve_data(calendar_query)
+        gantt_df = retrieve_data(gantt_query)
+
+        with col1:
+            calendar_options = {
+                "editable": "true",
+                "selectable": "true",
+                "headerToolbar": {
+                    "left": "today prev,next",
+                    "center": "title",
+                    "right": "timeGridDay,timeGridWeek,dayGridMonth",
+                },
+                "slotMinTime": "12:00:00",
+                "slotMaxTime": "24:00:00",
+                "initialView": "dayGridMonth",
+            }
+            calendar_events = [
+                {
+                    'title': ('W' if (
+                            record['game'].startswith('vs') 
+                            and record['home_score'] > record['away_score']) 
+                            or (record['game'].startswith('@') 
+                            and record['home_score'] < record['away_score'])
+                            else 'L') + ' ' + record['game'], 
+                    'start': record['game_date'],
+                    'backgroundColor': '#37752B' if (
+                            record['game'].startswith('vs') 
+                            and record['home_score'] > record['away_score']) 
+                            or (record['game'].startswith('@') 
+                            and record['home_score'] < record['away_score'])
+                            else '#F15F30',
+                    'borderColor': '#37752B' if (
+                            record['game'].startswith('vs') 
+                            and record['home_score'] > record['away_score']) 
+                            or (record['game'].startswith('@') 
+                            and record['home_score'] < record['away_score'])
+                            else '#F15F30'
+                    } for record in calendar_df.to_dict('records')
+            ]
+            custom_css="""
+                .fc-event-past {
+                    opacity: 0.8;
+                }
+                .fc-event-time {
+                    font-style: italic;
+                }
+                .fc-event-title {
+                    font-weight: 700;
+                }
+                .fc-toolbar-title {
+                    font-size: 2rem;
+                }
+            """
+            calendar = calendar(events=calendar_events, options=calendar_options, custom_css=custom_css)
+        with col2:
+            # Apply conversion to start and end times
+            st.dataframe(gantt_df)
+            gantt_df['stint_start_time'] = gantt_df['stint_start_time'].apply(time_to_timedelta)
+            gantt_df['stint_end_time'] = gantt_df['stint_end_time'].apply(time_to_timedelta)
+            reference_date = datetime(2024, 1, 1)
+            gantt_df['stint_start_time'] = reference_date + gantt_df['stint_start_time']
+            gantt_df['stint_end_time'] = reference_date + gantt_df['stint_end_time']
+
+            fig = px.timeline(gantt_df, x_start='stint_start_time', x_end='stint_end_time', y='player')
+            fig.update_layout(
+                xaxis_title="Time", yaxis_title="Player",
+                showlegend=True,
+                xaxis=dict(dtick=12, tickformat='%M:%S')
+                )
+            st.plotly_chart(fig, use_container_width=True)
     
     if st.button("Logout"):
         st.session_state.user_state['logged_in'] = False
